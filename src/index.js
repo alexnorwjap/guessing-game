2cf2496c590e1bf013175efbe877b3aea0392223
Aleks D.V. <alex.norw.jap@gmail.com>
2025-08-19 18:14:51 +0300

    feat: complete

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 class GuessingGame {
   constructor() {
-    throw new Error('Not implemented');
+    this.min = 0;
+    this.max = 0;
+    this.last = 0;
   }
-  /*
-  setRange(min, max) { }
 
-  guess() { }
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-  lower() { }
+  guess() {
+    this.last = Math.round((this.min + this.max) / 2);
+    return this.last;
+  }
 
-  greater() { }
-*/
+  lower() {
+    this.max = this.last;
+  }
+
+  greater() {
+    this.min = this.last;
+  }
 }
 
 module.exports = GuessingGame;
